118ffaf19fb67f458d8e9864f612d3b965a443f7
lguosu <165636012+lguosu@users.noreply.github.com>
2024-06-28 06:32:16 +0000

    Updated requirements and dockerfile.

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/larry')
+def hello_larry():
+    return 'Hello, larry!'
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- run_docker.sh
+++ run_docker.sh
@@ -10,4 +10,4 @@ docker image ls
 docker run -d -p 8080:8080 loadtest
 
 # debug
-docker run -it --entrypoint /bin/bash loadtest 
+docker run -it --entrypoint /bin/bash loadtest
